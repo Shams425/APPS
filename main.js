8b90fd50653905c5790556298de3916c74daef00
Shams425 <alexdavid486@gmail.com>
2023-05-01 21:33:56 +0200

    updating the UI ,adding score & adding  nextNumbers

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-//selection the columns
-const showCol = document.querySelectorAll(".col");
-const userSelect = document.querySelectorAll(".userSelect .columns .select")[
-  Math.floor(document.querySelectorAll(".select").length / 2)
-];
+const showCols = document.querySelectorAll(".col");
+const userSelect = document.querySelectorAll(".userSelect .columns .select");
+const nextNumBtn = document.querySelectorAll(".showNextNumbers .numCube");
+const userBtn = document.querySelectorAll(".col-user");
+const showScore = document.querySelector(".score");
+
+let nextRandNums = [];
+let userScore = 0;
+let cubeValue = 0;
 let allColumns = {
   col1: [],
   col2: [],
@@ -11,89 +15,157 @@ let allColumns = {
   col5: [],
 };
 
-//game functionality
-function getRandNum() {
-  return Math.pow(2, Math.floor(Math.random() * 5) + 1);
-  // return 8;
+// =========================================================================================
+// game functionality
+
+//show number when mouse is over
+userBtn.forEach((btn) => {
+  btn.addEventListener("mouseover", (el) => {
+    el.target.textContent = nextRandNums[nextRandNums.length - 1];
+  });
+
+  btn.addEventListener("mouseout", (el) => {
+    el.target.textContent = "";
+  });
+});
+
+function calcScore(num) {
+  userScore += parseInt(num);
+  showScore.textContent = "";
+  showScore.textContent += userScore;
 }
 
-function createUserCube() {
-  let randNum = getRandNum();
-  const div = document.createElement("div");
-  div.className = "userInput";
-  div.textContent = randNum;
+function getRandNum() {
+  // return Math.pow(2, Math.floor(Math.random() * 5) + 1);
+  return 4;
+}
 
-  userSelect.appendChild(div);
-
-  div.addEventListener("click", (e) => {
-    createNumCube(randNum);
+function showNextNumber() {
+  nextNumBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      createNumCube(e.target.textContent);
+    });
   });
 }
 
-function createNumCube(num) {
+function createNumCube() {
   const div = document.createElement("div");
   div.className = "numCube";
-  div.textContent = num;
+  div.textContent = nextNumBtn[nextNumBtn.length - 1].textContent;
+
+  nextRandNums.pop();
+  nextRandNums.unshift(getRandNum());
+
+  nextNumBtn.forEach((btn, index) => {
+    btn.textContent = nextRandNums[index];
+  });
 
   return div;
 }
 
+//get the whole numbers in a column and adding it to it's target col Object
 function getColData(num) {
   allColumns[`col${num}`] = [];
-  showCol[parseInt(num) - 1].childNodes.forEach((col) => {
-    allColumns[`col${num}`].push(col.textContent);
+  showCols[parseInt(num) - 1].childNodes.forEach((col) => {
+    allColumns[`col${num}`].push(parseInt(col.textContent));
   });
   meargeColNum(num);
-
-  console.log(allColumns[`col${num}`]);
 }
 
+// mearge the number of the col array
 function meargeColNum(col) {
-  if (allColumns[`col${col}`].length < 1) return;
+  if (allColumns[`col${col}`].length <= 1) return;
 
-  if (
-    allColumns[`col${col}`][allColumns[`col${col}`].length - 1] ===
-    allColumns[`col${col}`][allColumns[`col${col}`].length - 2]
-  ) {
-    let cubeValue =
-      parseInt(allColumns[`col${col}`][allColumns[`col${col}`].length - 1]) * 2;
+  allColumns[`col${col}`].reduce((_, nex, index) => {
+    if (nex === allColumns[`col${col}`][index - 1]) {
+      cubeValue = parseInt(nex) * 2;
 
-    allColumns[`col${col}`][allColumns[`col${col}`].length - 2] = cubeValue;
+      allColumns[`col${col}`][allColumns[`col${col}`].length - 2] = cubeValue;
 
-    allColumns[`col${col}`].pop();
+      allColumns[`col${col}`].pop();
 
-    meargeCube(col, cubeValue);
-  }
-}
-
-function meargeCube(colNum, colValue) {
-  showCol[parseInt(colNum) - 1].childNodes[
-    showCol[parseInt(colNum) - 1].childNodes.length - 1
-  ].remove();
-  showCol[parseInt(colNum) - 1].childNodes[
-    showCol[parseInt(colNum) - 1].childNodes.length - 1
-  ].textContent = colValue;
-}
-
-// showCol[0].parentElement
-//user select actions
-const userBtn = document.querySelectorAll(".col-user");
-
-userBtn.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    if (e.target.hasAttribute("id")) {
-      console.log("it's working");
-      showCol[e.target.id - 1].appendChild(createNumCube());
-      getColData(e.target.id);
-    } else {
-      console.log(e.target.parentElement);
-      showCol[e.target.parentElement.id - 1].appendChild(createNumCube());
-      getColData(e.target.id);
+      meargeCube(cubeValue, col);
     }
+  });
+  // rowMearge(col, cubeValue);
+}
+
+// mearge the cubes of the col
+function meargeCube(colValue, colNum) {
+  //remove the last cube
+  showCols[parseInt(colNum) - 1].childNodes[
+    showCols[parseInt(colNum) - 1].childNodes.length - 1
+  ].remove();
+
+  //channing the last cube number to the mearged value
+  showCols[parseInt(colNum) - 1].childNodes[
+    showCols[parseInt(colNum) - 1].childNodes.length - 1
+  ].textContent = colValue;
+
+  calcScore(colValue);
+  meargeColNum(colNum);
+}
+
+// function rowMearge(col, val) {
+//   let rowIndex = parseInt(allColumns[`col${col}`].length - 1);
+//   let meargeVal = 0;
+//   if (parseInt(col) === 0) col = 1;
+
+//   if (parseInt(col) <= 1) {
+//     if (
+//       allColumns[`col${col}`][rowIndex] ===
+//       allColumns[`col${parseInt(col) + 1}`][rowIndex]
+//     ) {
+//       meargeVal = parseInt(allColumns[`col${parseInt(col)}`][rowIndex]) * 2;
+
+//       showCols[parseInt(col)].childNodes[rowIndex].textContent = meargeVal;
+//       console.log(col);
+
+//       showCols[parseInt(col) - 1].childNodes[rowIndex].remove();
+//     }
+//   } else if (parseInt(col) > 3) {
+//     if (
+//       allColumns[`col${col}`][rowIndex] ===
+//       allColumns[`col${parseInt(col) - 1}`][rowIndex]
+//     ) {
+//       meargeVal = parseInt(allColumns[`col${parseInt(col)}`][rowIndex]) * 2;
+
+//       showCols[parseInt(col)].childNodes[rowIndex].textContent = meargeVal;
+//       console.log(col);
+
+//       showCols[parseInt(col) - 1].childNodes[rowIndex].remove();
+//     }
+//   } else if (
+//     allColumns[`col${col}`][rowIndex] ===
+//       allColumns[`col${parseInt(col) + 1}`][rowIndex] ||
+//     allColumns[`col${col}`][rowIndex] ===
+//       allColumns[`col${parseInt(col) - 1}`][rowIndex]
+//   ) {
+//     meargeVal = parseInt(allColumns[`col${parseInt(col)}`][rowIndex]) * 2;
+
+//     showCols[parseInt(col)].childNodes[rowIndex].textContent = meargeVal;
+//     console.log(col);
+
+//     showCols[parseInt(col) - 1].childNodes[rowIndex].remove();
+//   }
+// }
+
+// =========================================================================================
+// game start
+window.addEventListener("load", () => {
+  for (i = 0; i < 3; i++) {
+    nextRandNums.push(getRandNum());
+  }
+
+  nextNumBtn.forEach((btn, index) => {
+    btn.textContent = nextRandNums[index];
+    // rowMearge(index);
   });
 });
 
-// game start
-window.addEventListener("load", () => {
-  createUserCube();
+userBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    showCols[e.target.id - 1].appendChild(createNumCube());
+    getColData(e.target.id);
+  });
 });
